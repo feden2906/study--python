cf4205e3021af88c9a1dc1c8d712fb3dad28e68c
Fedenko Maksym <feden2906@gmail.com>
2021-04-09 21:36:16 +0300

    string

--- lesson2.py
+++ lesson2.py
@@ -37,3 +37,49 @@
 
 #
 
+
+# # # STRING # # #
+name = 'Max'
+age = 25
+weight = 75.5
+#
+# string = 'Hello, my name is ' + name + ', I`m ' + str(age) + ' and my weight - ' + str(weight) + ' kg.'
+# string = 'Hello, my name is %s, I`m %d and my weight - %f kg.' % (name, age, weight)
+# string = 'Hello, my name is {}, I`m {} and my weight - {} kg.'.format(name, age, weight)
+# string = f'Hello, my name is {name}, I`m {age} and my weight - {weight} kg.'
+# print(string)
+
+# print(string.index('Ma', 2, 12))
+# print(string.find('l'))
+# print(string.count('e', 5, 55))
+# print(string.capitalize())
+# print(string.upper())
+# print(string.lower())
+# print(string.title())
+# print(string.swapcase())
+
+string = '-     aaBadaafagah aDad  a - fag -ha,a a-kaFfagaah-alal'
+# print(string.isalpha())
+# print(string.isdigit())
+# print(string.isalnum())
+# print(string.isspace())
+# print('5.5'.isdecimal())
+# print(string.isdecimal())
+# print(string.istitle())
+
+# print(string.startswith('ll'))
+# print(string.endswith('ll'))
+
+# print(string.strip())
+# print(string.split(' '))
+# print(string.partition('-'))
+
+# li = ['apple', 'car', 'user']
+# string = ' * '.join(li)
+# print(string)
+
+# print(string.replace('ll', 'ss', 1))
+
+
+#
+
