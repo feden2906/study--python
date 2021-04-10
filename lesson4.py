8d17f10407119325cbea147abdb67dd344a4f4ea
Fedenko Maksym <feden2906@gmail.com>
2021-04-10 20:29:21 +0300

    imdestructuring

---
+++ lesson4.py
@@ -0,0 +1,28 @@
+# # # destructuring # # #
+# tuple1 = (1, 2, 3, 4, 5, 6)
+# a, b, *other = tuple1
+# print(a)
+# print(b)
+# print(other)
+
+# a = 5
+# b = 6
+# a, b = b, a
+# a, b = (b, a)
+# print(a)
+# print(b)
+
+# def function(a='a', b='b', c='c'):
+#     return (a, b, c)
+#
+# print(function(c=1))
+
+# def function(a='a', b='b', c='c'):
+#     return (a, b, c)
+#
+# dict1 = {'a':1, 'b':5}
+# print(function(**dict1))
+
+
+
+
