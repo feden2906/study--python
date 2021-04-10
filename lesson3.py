ac7684dd2d394c1d75af8cf88d2a34aaf742004e
Fedenko Maksym <feden2906@gmail.com>
2021-04-10 10:59:31 +0300

    zip

---
+++ lesson3.py
@@ -0,0 +1,12 @@
+# # # ZIP # # #
+# l1 = ['Maks', 'Karina', 'Anton']
+# l2 = [14, 15, 45]
+# l3 = [11, 22, 33]
+# data1 = dict(zip(l1, l2))
+# data2 = tuple(zip(l1, l2, l3))
+# print(data1)
+# print(data2)
+
+
+#
+
