9e1eb6260d7c53fe806ddac8389ec6eea97141c0
Fedenko Maksym <feden2906@gmail.com>
2021-04-11 19:59:20 +0300

    приватність

---
+++ lesson5.py
@@ -0,0 +1,13 @@
+# # # приватність # # #
+# class Animal:
+#     _dich = 0                 # протект змінна
+#     __count = 0               # приватна змінна
+#
+#     def __init__(self, name):
+#         self.__name = name
+#
+# animal = Animal('Maksym')
+# print(animal._Animal__name)
+
+
+#
