71c5c759ec1ed2de028d4bd877bc1a5533640f9d
Fedenko Maksym <feden2906@gmail.com>
2021-04-12 15:26:45 +0300

    try

---
+++ lesson6.py
@@ -0,0 +1,25 @@
+# # # try # # #
+# num = 0
+#
+# try:
+#     result = 23456 / num
+#     if num == 2:
+#         raise Exception('message')
+#
+# except ValueError as value_err:
+#     print(value_err)
+#
+# except ZeroDivisionError as zero_err:
+#     print(zero_err)
+#
+# except Exception as err:
+#     print(err)
+#
+# else:
+#     print('Спрацьовує якщо блок try виконався успішно')
+#
+# finally:
+#     print('Спрацьовує в будь-якому разі')
+
+
+#
