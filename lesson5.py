6a56279d88f38818894b9989fd33f7e039dbcf8d
Fedenko Maksym <feden2906@gmail.com>
2021-04-12 09:55:30 +0300

    @staticmethod

--- lesson5.py
+++ lesson5.py
@@ -184,3 +184,22 @@
 
 #
 
+
+# # # @staticmethod # # #
+# class User:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def __str__(self):
+#         return f'{self.__dict__}'
+#
+#     @classmethod
+#     def init(cls, name, age):
+#         return cls(name, age)
+#
+#
+# print(User.init('Maksym', 25))
+
+
+#
