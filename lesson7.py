0deac2a7f9501a10bdd2a47ad3334cc4a3941a90
Fedenko Maksym <feden2906@gmail.com>
2021-04-14 15:50:59 +0300

    venv(віртуальне середовище розробки)

--- lesson7.py
+++ lesson7.py
@@ -54,3 +54,32 @@
 #
 
 
+class User:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+from typing import List
+
+def create_user(arr1: List[str], arr2: List[int]) -> List[User]:
+    users: List[User] = []
+    for i in range(len(arr1)):
+        users.append(User(arr1[i].title(), arr2[i]))
+
+    return users
+
+
+names = ['maksym', 'Tamara', 'Alina', 'Dimas']
+ages = [25, 16, 27, 23]
+
+print(create_user(names, ages))
+
+# перевірку типів робимо командою - mypy lesson7.py
+# попередньо mypy встановлене в venv
